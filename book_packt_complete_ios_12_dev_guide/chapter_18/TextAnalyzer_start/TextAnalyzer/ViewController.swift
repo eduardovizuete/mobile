36fe86787af85cbc71dfdb6bed3bde823169ba43
Eduardo Vizuete <gilberto.vizuete@modinter.com.ec>
2019-05-21 10:01:33 -0500

    chapter 18: smarter apps with coreml - using a coreml model

--- book_packt_complete_ios_12_dev_guide/chapter_18/TextAnalyzer_start/TextAnalyzer/ViewController.swift
+++ book_packt_complete_ios_12_dev_guide/chapter_18/TextAnalyzer_start/TextAnalyzer/ViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Vision
+import NaturalLanguage
 
 class ViewController: UIViewController {
   
@@ -14,6 +15,30 @@ class ViewController: UIViewController {
   }
   
   @IBAction func analyze() {
+    let wordCount = getWordCounts(from: textView.text)
+    let model = SentimentPolarity()
     
+    guard let prediction = try? model.prediction(input: wordCount)
+      else { return }
+    
+    let alert = UIAlertController(title: nil, message: "Your text is rated: \(prediction.classLabel)", preferredStyle: .alert)
+    let okayAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+    alert.addAction(okayAction)
+    present(alert, animated: true, completion: nil)
   }
+}
+
+func getWordCounts(from string: String) ->[String: Double] {
+  let tokenizer = NLTokenizer(unit: .word)
+  tokenizer.string = string
+  var wordCount = [String: Double]()
+  
+  tokenizer.enumerateTokens(in: string.startIndex..<string.endIndex) { range, attributes in
+    let word = String(string[range])
+    wordCount[word] = (wordCount[word] ?? 0) + 1
+    
+    return true
+  }
+  
+  return wordCount
 }
