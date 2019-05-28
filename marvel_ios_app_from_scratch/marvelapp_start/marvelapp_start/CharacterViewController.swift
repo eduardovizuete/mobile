3bc950997579919d71cc83e4a5f42a7b2cfce201
Eduardo Vizuete <gilberto.vizuete@modinter.com.ec>
2019-05-28 13:00:03 -0500

    Creating initial view controllers

---
+++ marvel_ios_app_from_scratch/marvelapp_start/marvelapp_start/CharacterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterViewController.swift
+//  marvelapp_start
+//
+//  Created by eduardo on 5/28/19.
+//  Copyright © 2019 eduardo. All rights reserved.
+//
+
+import UIKit
+
+class CharacterViewController: UIViewController {
+  
+}
---
+++ marvel_ios_app_from_scratch/marvelapp_start/marvelapp_start/CharactersViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CharactersViewController.swift
+//  marvelapp_start
+//
+//  Created by eduardo on 5/28/19.
+//  Copyright © 2019 eduardo. All rights reserved.
+//
+
+import UIKit
+
+class CharactersViewController: UIViewController {
+  
+}
