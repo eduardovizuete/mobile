4df174c4adb2ef992e1d886e331160e5e7e37250
Eduardo Vizuete <gilberto.vizuete@modinter.com.ec>
2019-05-28 12:01:22 -0500

    Creating initial project structure

---
+++ marvel_ios_app_from_scratch/marvelapp_start/marvelapp_start/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  marvelapp_start
+//
+//  Created by eduardo on 5/28/19.
+//  Copyright © 2019 eduardo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+  
+}
+
